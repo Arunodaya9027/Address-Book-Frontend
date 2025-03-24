57ef19bf102396876eba948623c4de369c58f1c3
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-03-24 16:38:16 +0530

    [Arunodaya Pratap Singh] Modified.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 // import { AuthComponent } from "./components/auth/auth.component";
 import { AddressBookComponent } from "./components/address-book/address-book.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AddressBookComponent],
+  imports: [AddressBookComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
