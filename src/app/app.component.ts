a0ef3e8f4c18a244afe13dea4b609edfecbcb251
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-03-24 16:18:16 +0530

    [Arunodaya Pratap Singh] Add. Create the Address Book frontend View with CRUD operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { AuthComponent } from "./components/auth/auth.component";
+import { AddressBookComponent } from "./components/address-book/address-book.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AddressBookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AddressBook-Frontend';
+  imageUrl = './assets/Logo.png';
 }
